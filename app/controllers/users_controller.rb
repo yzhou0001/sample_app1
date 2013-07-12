4d7f8b2c941da3804bd7fde8cd89498d55eaba2d
Ying Zhou <yzhou0001@yahoo.com>
2013-07-12 17:05:40 -0400

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   
@@ -54,12 +55,7 @@ def destroy
  #   def signed_in_user
  #     redirect_to signin_url, notice: "Please sign in." unless signed_in?
  #  end
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+
     
     def correct_user
       @user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
 
   # before_save { |user| user.email = email.downcase }
   before_save { email.downcase! }
@@ -25,6 +26,11 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   after_validation { self.errors.messages.delete(:password_digest) }
+  
+ def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end 
 
 private
 
